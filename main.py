93420ef91356745e467733cfb023c73eec484148
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-21 18:19:14 -0300

    feat: implemented get list from user

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import sort_by_field_mal as logic_code
 import time
 import winsound
 
+USERNAME_INPUT_WINDOW_TEXT = "Enter your username, your list needs to be public"
+
 RELEASE_YEAR = 5
 
 
@@ -42,137 +44,143 @@ POSSIBLE_FIELDS = {
 MAL_DATASET_CSV = "MAL_dataset_Winter_2022.csv"
 
 def createMainWindow():
-    layout = [
-        [PySimpleGUI.Text("Select the desired field to sort")],
-        [PySimpleGUI.Button("Sort by members"), PySimpleGUI.Button("Sort by global score"),
-         PySimpleGUI.Button("Sort by amount of episodes"), PySimpleGUI.Button("Sort by release year"),
-         PySimpleGUI.Button("Sort anime titles alphabetically")
-         ],
-        [PySimpleGUI.Multiline(autoscroll=True, size=(900, 600), auto_refresh=True, reroute_stdout=True)]
-    ]
+  layout = [
+      [PySimpleGUI.Text("Select the desired field to sort")],
+      [PySimpleGUI.Button("Sort by members"), PySimpleGUI.Button("Sort by global score"),
+        PySimpleGUI.Button("Sort by amount of episodes"), PySimpleGUI.Button("Sort by release year"),
+        PySimpleGUI.Button("Sort anime titles alphabetically")
+        ],
+      [PySimpleGUI.Multiline(autoscroll=True, size=(900, 600), auto_refresh=True, reroute_stdout=True)]
+  ]
 
-    return PySimpleGUI.Window("MAL account sorter", layout, size=(1000, 600))
+  return PySimpleGUI.Window("MAL account sorter", layout, size=(1000, 600))
 
 
 def main():
 
-    malAccountPath = askForMalAccountPath()
-    if malAccountPath is None:
-        return
-    window = createMainWindow()
+  username, animeList = askForUserList()
+  if(username == None):
+    return
+  print(username)
+  window = createMainWindow()
 
-    while True:
-        event, values = window.read()
-        if event == PySimpleGUI.WIN_CLOSED:
-            break
-        try:
-            desiredField, nameOfTheField = POSSIBLE_FIELDS[event]
-        except KeyError:
-            print("Again!")
-            continue
+  while True:
+    event, values = window.read()
+    if event == PySimpleGUI.WIN_CLOSED:
+      break
+    try:
+      desiredField, nameOfTheField = POSSIBLE_FIELDS[event]
+    except KeyError:
+      print("Again!")
+      continue
 
-        sortedAccount = logic_code.sortMalAccountByDesiredField(malAccountPath, MAL_DATASET_CSV, desiredField)
-        logic_code.cutePrint(sortedAccount, nameOfTheField)
-        askForFileOut(sortedAccount, nameOfTheField)
-        print("Done!")
-    window.close()
+    sortedAccount = logic_code.sortMalAccountByDesiredField(malAccountPath, MAL_DATASET_CSV, desiredField)
+    logic_code.cutePrint(sortedAccount, nameOfTheField)
+    askForFileOut(sortedAccount, nameOfTheField)
+    print("Done!")
+  window.close()
 
 
-def askForMalAccountPath():
-
-    window = createAskForMalAccountWindow()
-    path = None
-    while True:
-        event, values = window.read()
-        if event == PySimpleGUI.WIN_CLOSED:
-            break
-        if event == 'Submit':
-            if values["Browse"] == "":
-                winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-                continue
-            if not checkIfThePathIsValid(values["Browse"]):
-                winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-                continue
-
-            path = values["Browse"]
-            window.close()
-
-    return path
-
+def askForUserList():
+  
+  window = createAskForUserListWindow()
+  while True:
+    event, values = window.read()
+    if event == PySimpleGUI.WIN_CLOSED:
+      return None, None
+    if event == "usernameInput" + "_Enter":
+      if values["usernameInput"] == "":
+        winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+        continue
+      username = values["usernameInput"]
+      
+      window["userWindowText"].update(value="Loading your list...")
+      window.refresh()
+      animeList = logic_code.getListFromUser(username)
+      window["userWindowText"].update(value=USERNAME_INPUT_WINDOW_TEXT)
+      window.refresh()
+      
+      if not animeList:
+        winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+        PySimpleGUI.popup("The list is set to private or the username doesn't exist")
+        continue
+      
+      window.close()
+    
+      return username, animeList
 
 def checkIfThePathIsValid(path):
-    extension = path[len(path) - 3:]
-    return extension == "xml"
+  extension = path[len(path) - 3:]
+  return extension == "xml"
 
 
-def createAskForMalAccountWindow():
-    layout = [
-        [PySimpleGUI.Text("Choose file", size=(100, None), justification="center")],
-        [PySimpleGUI.In(), PySimpleGUI.FileBrowse()],
-        [PySimpleGUI.Button("Submit")]
-    ]
-    window = PySimpleGUI.Window("Select your exported mal account in xml", layout, size=(500, 100),
-                                element_justification='c')
-    return window
-
-
+def createAskForUserListWindow():
+  layout = [
+      [PySimpleGUI.Text(USERNAME_INPUT_WINDOW_TEXT, justification="center", key="userWindowText")],
+      [PySimpleGUI.Input(key="usernameInput")]
+  ]
+  window = PySimpleGUI.Window("Enter your username", layout, size=(500, 100),
+                              element_justification='c', finalize=True)
+  window["usernameInput"].bind("<Return>", "_Enter")
+  return window
+  
 def askForFileOut(sortedAccount, nameOfTheField):
-    layout = [
-        [PySimpleGUI.Text("Fileout to a text file?", size=(100, None), justification="center")],
-        [PySimpleGUI.Button("Yes"), PySimpleGUI.Button("No")]
-    ]
-    window = PySimpleGUI.Window("Fileout choice", layout, size=(250, 80), element_justification='c', modal=True)
+  layout = [
+      [PySimpleGUI.Text("Fileout to a text file?", size=(100, None), justification="center")],
+      [PySimpleGUI.Button("Yes"), PySimpleGUI.Button("No")]
+  ]
+  window = PySimpleGUI.Window("Fileout choice", layout, size=(250, 80), element_justification='c', modal=True)
 
-    while True:
-        event, values = window.read()
+  while True:
+    event, values = window.read()
 
-        if event == PySimpleGUI.WIN_CLOSED:
-            break
-        if event == "Yes":
-            if not outputToFile(sortedAccount, nameOfTheField):
-                window.close()
-                return
-            print("Saving")
-            time.sleep(0.5)
+    if event == PySimpleGUI.WIN_CLOSED:
+      break
+    if event == "Yes":
+      if not outputToFile(sortedAccount, nameOfTheField):
         window.close()
+        return
+      print("Saving")
+      time.sleep(0.5)
+    window.close()
 
 
 def askForOutputPath():
 
-    window = createAskForOutputPathWindow()
+  window = createAskForOutputPathWindow()
 
-    path = None
-    while True:
-        event, values = window.read()
-        if event == PySimpleGUI.WIN_CLOSED:
-            break
-        if event == 'Submit':
-            if values["Browse"] == "":
-                winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
-                continue
+  path = None
+  while True:
+    event, values = window.read()
+    if event == PySimpleGUI.WIN_CLOSED:
+      break
+    if event == 'Submit':
+      if values["Browse"] == "":
+        winsound.PlaySound("Windows Ding.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+        continue
 
-            path = values["Browse"]
-            window.close()
+      path = values["Browse"]
+      window.close()
 
-    return path
+  return path
 
 
 def outputToFile(sortedAccount, nameOfTheField):
-    fileExitPath = askForOutputPath()
-    if fileExitPath == None:
-        return False
-    completeExitPath = fileExitPath + "/Account ordered by " + nameOfTheField + ".txt"
-    logic_code.writeOnFileTheOrderedList(sortedAccount, completeExitPath, nameOfTheField)
-    return True
+  fileExitPath = askForOutputPath()
+  if fileExitPath == None:
+    return False
+  completeExitPath = fileExitPath + "/Account ordered by " + nameOfTheField + ".txt"
+  logic_code.writeOnFileTheOrderedList(sortedAccount, completeExitPath, nameOfTheField)
+  return True
 
 
 def createAskForOutputPathWindow():
-    layout = [
-        [PySimpleGUI.Text("Choose the exit folder", size=(100, None), justification="center")],
-        [PySimpleGUI.In(), PySimpleGUI.FolderBrowse()],
-        [PySimpleGUI.Button("Submit")]
-    ]
-    window = PySimpleGUI.Window("Fileout choice", layout, size=(500, 100), element_justification='c', modal=True)
-    return window
+  layout = [
+      [PySimpleGUI.Text("Choose the exit folder", size=(100, None), justification="center")],
+      [PySimpleGUI.In(), PySimpleGUI.FolderBrowse()],
+      [PySimpleGUI.Button("Submit")]
+  ]
+  window = PySimpleGUI.Window("Fileout choice", layout, size=(500, 100), element_justification='c', modal=True)
+  return window
 
 main()
--- sort_by_field_mal.py
+++ sort_by_field_mal.py
@@ -1,116 +1,123 @@
 import csv
 import xml.etree.ElementTree as ET
+import requests
 
-def malToSet(malXml):
-    malSet = set()
+CRED_FILE = "credentials"
 
-    tree = ET.parse(malXml)
-    root = tree.getroot()
+def getClientID():
+  with open(CRED_FILE, "r") as credFile:
+    return credFile.readline()
 
-    for anime in root.findall('anime'):
+CLIENT_ID = getClientID()
 
-        malEntryTitle = anime.find('series_title').text
-        malEntryStatus = anime.find('my_status').text
-
-        if malEntryStatus == "Completed":
-            malSet.add(malEntryTitle)
-
-    return malSet
+def getListFromUser(username: str):
+  url = f'https://api.myanimelist.net/v2/users/{username}/animelist?status=completed&limit=1000&fields=id,title,mean,popularity,num_list_users,num_scoring_users,list_status,start_season'
+  response = requests.get(url, headers={
+    'X-MAL-CLIENT-ID': CLIENT_ID
+  })
+  try:
+    response.raise_for_status()
+  except:
+    return None
+  animeList = response.json()
+  response.close()
+  
+  return animeList
 
 
 def createDictWithAnimesAndSelectedField(malCsvDataset, malAccountSet, desiredField):
-    titleAndField = {}
+  titleAndField = {}
 
-    with open(malCsvDataset, "r", encoding="utf-8") as malDatasetFile:
+  with open(malCsvDataset, "r", encoding="utf-8") as malDatasetFile:
 
-        malDatasetReader = csv.reader(malDatasetFile)
-        next(malDatasetReader, None)
+    malDatasetReader = csv.reader(malDatasetFile)
+    next(malDatasetReader, None)
 
-        for malEntry in malDatasetReader:
+    for malEntry in malDatasetReader:
 
-            if malEntry == []:
-                continue
+      if malEntry == []:
+        continue
 
-            if desiredField == "anime title":
-                if malEntry[0] in malAccountSet:
-                    titleAndField[malEntry[0]] = "anime title"
-                continue
+      if desiredField == "anime title":
+        if malEntry[0] in malAccountSet:
+            titleAndField[malEntry[0]] = "anime title"
+        continue
 
-            malEntryTitle = malEntry[0]
-            malEntryField = malEntry[desiredField]
+      malEntryTitle = malEntry[0]
+      malEntryField = malEntry[desiredField]
 
-            if malEntryTitle in malAccountSet:
-                titleAndField[malEntryTitle] = float(malEntryField)
+      if malEntryTitle in malAccountSet:
+        titleAndField[malEntryTitle] = float(malEntryField)
 
-    return titleAndField
+  return titleAndField
 
 
 def sortMalAccountByDesiredField(malAccount, malDatabase, desiredField):
-    accountToSet = malToSet(malAccount)
-    entriesWithSelectedField = createDictWithAnimesAndSelectedField(malDatabase, accountToSet, desiredField)
-    if desiredField == "anime title":
-        return sorted(entriesWithSelectedField.items(), key=lambda x: x[0])
-    return sorted(entriesWithSelectedField.items(), key=lambda x: x[1], reverse=True)
+  accountToSet = malToSet(malAccount)
+  entriesWithSelectedField = createDictWithAnimesAndSelectedField(malDatabase, accountToSet, desiredField)
+  if desiredField == "anime title":
+    return sorted(entriesWithSelectedField.items(), key=lambda x: x[0])
+  return sorted(entriesWithSelectedField.items(), key=lambda x: x[1], reverse=True)
 
 
 def cutePrint(listOfTuples, nameOfTheField):
+  i = 0
+
+  for title, field in listOfTuples:
+    i += 1
+    if nameOfTheField == "anime title":
+      print(f"{i}. Title: {title}")
+      continue
+    print(f"{i}. Title: {title} | {nameOfTheField}: {field}")
+
+
+def writeOnFileTheOrderedList(listOfTuples, fileName, nameOfTheField):
+  with open(fileName, "w", encoding='utf-8') as file:
     i = 0
 
     for title, field in listOfTuples:
-        i += 1
-        if nameOfTheField == "anime title":
-            print(f"{i}. Title: {title}")
-            continue
-        print(f"{i}. Title: {title} | {nameOfTheField}: {field}")
-
-
-def writeOnFileTheOrderedList(listOfTuples, file_name, nameOfTheField):
-    with open(file_name, "w", encoding='utf-8') as file:
-        i = 0
-
-        for title, field in listOfTuples:
-            i += 1
-            if nameOfTheField == "anime title":
-                file.write(f"{i}. Title: {title}\n")
-                continue
-            file.write(f"{i}. Title: {title} | {nameOfTheField}: {field}\n")
+      i += 1
+      if nameOfTheField == "anime title":
+        file.write(f"{i}. Title: {title}\n")
+        continue
+      file.write(f"{i}. Title: {title} | {nameOfTheField}: {field}\n")
 
 # Command line code
 
 def printTheMainScreen():
-    print()
-    print("Posibles campos a ingresar")
-    print("-Puntuacion global")
-    print("-Año de salida")
-    print("-Cantidad de episodios")
-    print("-Cantidad de miembros")
+  print()
+  print("Posibles campos a ingresar")
+  print("-Puntuacion global")
+  print("-Año de salida")
+  print("-Cantidad de episodios")
+  print("-Cantidad de miembros")
 
-    print()
-    print("Ingresa 'salir' para salir")
-    print()
+  print()
+  print("Ingresa 'salir' para salir")
+  print()
 
 
 def main_for_command_line():
-    while True:
+  while True:
 
-        printTheMainScreen()
-        campoIngresado = input("Ingresá el campo deseado para ordenar tu mal: ").lower()
+    printTheMainScreen()
+    campoIngresado = input("Ingresá el campo deseado para ordenar tu mal: ").lower()
 
-        if campoIngresado == "salir":
-            break
+    if campoIngresado == "salir":
+      break
 
-        try:
-            desiredField, nameOfTheField = POSIBLES_CAMPOS[campoIngresado]
-        except KeyError:
-            print("Otra vez")
-            continue
+    try:
+      desiredField, nameOfTheField = POSIBLES_CAMPOS[campoIngresado]
+    except KeyError:
+      print("Otra vez")
+      continue
 
-        sortedAccount = sortMalAccountByDesiredField("myAnimeList.csv", "MAL_dataset_Spring_2021.csv", desiredField)
-        cutePrint(sortedAccount, nameOfTheField)
-        print("Querés exportarlo a un archivo .txt?")
-        exportChoice = input("Y/N: ").lower()
+    sortedAccount = sortMalAccountByDesiredField("myAnimeList.csv", "MAL_dataset_Spring_2021.csv", desiredField)
+    cutePrint(sortedAccount, nameOfTheField)
+    print("Querés exportarlo a un archivo .txt?")
+    exportChoice = input("Y/N: ").lower()
 
-        if exportChoice == "y":
-            writeOnFileTheOrderedList(sortedAccount, "accountOrderedBy" + nameOfTheField + ".txt", nameOfTheField)
-            print("Done!")
-            print()
+    if exportChoice == "y":
+      writeOnFileTheOrderedList(sortedAccount, "accountOrderedBy" + nameOfTheField + ".txt", nameOfTheField)
+      print("Done!")
+      print()
